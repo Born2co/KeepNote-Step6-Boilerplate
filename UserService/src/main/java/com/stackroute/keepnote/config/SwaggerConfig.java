df6e1ee0be1e6cf44a9fd5768a9112e0768a80d1
Kumar <725153@cognizant.com>
2019-04-01 11:24:30 +0530

    first commit

---
+++ UserService/src/main/java/com/stackroute/keepnote/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.stackroute.keepnote.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/*As in this class we are implementing Swagger So annotate the class with @Configuration and 
+ * @EnableSwagger2
+ * 
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+	/*
+	 * Annotate this method with @Bean . This method will return an Object of
+	 * Docket. This method will implement logic for swagger
+	 */
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.stackroute.keepnote")).paths(regex("/api/v1/user.*"))
+				.build();
+				
+	}
+	
+	
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                        .addResourceLocations("classpath:/META-INF/resources/");
+ 
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
